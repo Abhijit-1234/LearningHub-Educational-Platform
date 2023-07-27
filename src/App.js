40b2845844c66f7625af459fc93e305206aec0db
Abhijit-1234 <abhijitdutta31102001@gmail.com>
2023-07-27 17:02:31 +0530

    add logo

--- src/App.js
+++ src/App.js
@@ -5,22 +5,15 @@ import {
 import {Home, SingleCourse, Cart, Courses} from "./pages";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-// import login from './components/login';
 import { BiLogIn } from 'react-icons/bi';
-import login from './components/login';
-// import signup from "./components/signup";
-// import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Sidebar />
-      <login/>
       <Routes>
-        <Route path = "/" element = {<login/>} />
-        {/* <Route path='/signup' element = {<signup />}/> */}
-        <Route path='/home' element = {<Home />} />
+        <Route path='/' element = {<Home />} />
         <Route path = "/courses/:id" element = {<SingleCourse />} />
         <Route path = "/category/:category" element = {<Courses />} />
         <Route path = "/cart" element = {<Cart />} />
